77f875acaf344d8a49207e1e21ea01eb83afa4c0
anamavadu <anamavadu@gmail.com>
2024-08-10 12:01:13 -0500

    4_Cuarto: Configuración Módulo Admin

--- personal/empleados/urls.py
+++ personal/empleados/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
 
-
+path('empleados/', views.lista_empleados, name='empleados'),
+    path('empleados/<int:empleado_id>/', views.detalle, name='detalle'),
+    
 ]
